26f31994bc5b1404da777e5e02ef92eb2d71d2ff
Salomé Lobo <salomelobo.mkt@gmail.com>
2024-05-07 11:44:18 +0000

    Generate One Model per Source

--- models/staging/raw/stg_raw__sales.sql
+++ models/staging/raw/stg_raw__sales.sql
@@ -1,22 +1,7 @@
-with 
+with
 
-source as (
+    source as (select * from {{ source("raw", "sales") }}),
 
-    select * from {{ source('raw', 'sales') }}
-
-),
-
-renamed as (
-
-    select
-        date_date,
-        orders_id,
-        pdt_id,
-        revenue,
-        quantity
-
-    from source
-
-)
+    renamed as (select date_date, orders_id, pdt_id as products_id, revenue, quantity from source)
 
 select * from renamed
