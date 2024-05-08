28f7dd647732b46cd26a073a8dcc3c3af79b60ed
Salomé Lobo <salomelobo.mkt@gmail.com>
2024-05-08 10:20:38 +0000

    Project Structure challenge

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,10 +1,15 @@
-select
-    orders_id,
-    date_date,
-    round(margin + shipping_fee - logcost - cast(ship_cost as float64), 2) as operational_margin,
-    margin
-
-from {{ ref("int_orders_margin")}}
-join {{ ref("stg_raw__ship")}}
-using (orders_id)
-order by orders_id desc
+SELECT
+  o.orders_id,
+  o.date_date,
+  ROUND(cast(o.margin as float64) + cast(s.shipping_fee as float64) - (cast(s.logcost as float64) + cast(s.ship_cost as float64)),2) AS operational_margin, 
+  o.quantity,
+  o.revenue,
+  o.purchase_cost,
+  o.margin,
+  s.shipping_fee,
+  s.logcost,
+  s.ship_cost
+FROM {{ref("int_orders_margin")}} o
+LEFT JOIN {{ref("stg_raw__ship")}} s 
+  USING(orders_id)
+ORDER BY orders_id desc
