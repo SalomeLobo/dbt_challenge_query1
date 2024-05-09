d2c02fc9f6440f656542376ae2ba851d0f6282db
Salomé Lobo <salomelobo.mkt@gmail.com>
2024-05-09 07:32:53 +0000

    Finance campaigns by day and month

--- models/mart/finance/finance_campaigns_day.sql
+++ models/mart/finance/finance_campaigns_day.sql
@@ -1,12 +1,12 @@
 select
     date_date,
     round(operational_margin - ads_cost,2) as ads_margin,
-    average_basket,
+    ROUND(average_basket,2) AS average_basket,
     operational_margin,
     ads_cost
 
 from {{ ref("int_campaigns_day")}}
-join {{ ref("finance_days")}}
+full outer join {{ ref("finance_days")}}
 using (date_date)
 
 order by date_date desc
